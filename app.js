a37a8b48f5847937c938046be99ae6f06a31e9c1
SeminVsevolod <semin.vsevolod@gmail.com>
2020-06-15 02:13:43 +0300

    handling POST request

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const fs = require('fs');
-
 const app = express();
+
+/**
+ * Добавить промежуточный обработчик (middleware), который дает нам доступ к req.body в виде JSON
+ */
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res
@@ -13,10 +17,16 @@ const app = express();
 //   res.send('You can post to this endpoing...');
 // });
 
+/**
+ * Загрузить список туров из файла
+ */
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+/**
+ * Получить список всех туров
+ */
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -25,6 +35,36 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+/**
+ * Добавить новый тур
+ */
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  /**
+   * Перезаписывам JSON файл с турами (который используем в качестве фиктивной БД)
+   */
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      /**
+       * Статус 201 озн. "ресурс создан"
+       */
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
