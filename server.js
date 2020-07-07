4e92d8082368f8e3661c65f79f309af7f124204e
SeminVsevolod <semin.vsevolod@gmail.com>
2020-07-07 13:04:44 +0300

    install mongoose and confugure database connection in server.js

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
+/**
+ * +--------------------+
+ * | INIT CONFIGURATION |
+ * +--------------------+
+ */
 dotenv.config({ path: './config.env' });
+
+/**
+ * +---------------------+
+ * | DATABASE CONNECTION |
+ * +---------------------+
+ */
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+// const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((connection) =>
+    console.log(
+      `DB connection successful -> ${connection.connections[0].host}:${connection.connections[0].port}`
+    )
+  )
+  .catch((err) => console.log(`DB connection failed -> ${err.message}`));
 
 /**
  * +---------------+
