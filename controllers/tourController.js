2ee515557bc80ed85c1132d8d9cf18a82fe350f9
SeminVsevolod <semin.vsevolod@gmail.com>
2020-07-07 20:21:33 +0300

    refactoring for MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,43 +1,17 @@
-const fs = require('fs');
-
-/**
- * Загрузить список туров из файла
- */
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-/**
- * Проверить наличие id в req.params
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
- * @param {*} val 
- */
-exports.checkID = (req, res, next, val) => {
-  const tour = tours.find((el) => el.id === +req.params.id);
-  // Если тур не найден, ответить 404 с ошибкой
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+const Tour = require('../models/tourModel');
 
 /**
  * Проверить body на наличие name или price
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
  */
 exports.checkBody = (req, res, next) => {
   // Если тур не найден, ответить 404 с ошибкой
   if (!req.body.price || !req.body.name) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Name or price is missed'
+      message: 'Name or price is missed',
     });
   }
   next();
@@ -51,10 +25,10 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
@@ -62,70 +36,53 @@ exports.getAllTours = (req, res) => {
  * Получить тур по id
  */
 exports.getTour = (req, res) => {
-  const id = +req.params.id;
-  const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  // const id = +req.params.id;
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
 /**
  * Добавить новый тур
  */
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
   /**
-   * Перезаписывам JSON файл с турами (который используем в качестве фиктивной БД)
+   * Статус 201 озн. "ресурс создан"
    */
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      /**
-       * Статус 201 озн. "ресурс создан"
-       */
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    // status: 'success',
+    // data: {
+    //   tour: newTour,
+    // },
+  });
 };
 
 /**
  * Обновить существующий тур
  */
 exports.updateTour = (req, res) => {
-  const id = +req.params.id;
-  const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here>',
-    },
-  });
+  // const id = +req.params.id;
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour: '<Updated tour here>',
+  //   },
+  // });
 };
 
 /**
  * Удалить существующий тур
  */
 exports.deleteTour = (req, res) => {
-  const id = +req.params.id;
-  const tour = tours.find((el) => el.id === id);
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+  // const id = +req.params.id;
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(204).json({
+  //   status: 'success',
+  //   data: null,
+  // });
 };
--- server.js
+++ server.js
@@ -32,41 +32,6 @@ mongoose
   )
   .catch((err) => console.log(`DB connection failed -> ${err.message}`));
 
-// Описываем схему для туров
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-// Создаем модель тура на основе описанной ранее схемы
-const Tour = mongoose.model('Tour', tourSchema);
-
-// Создаем экземпляр тура
-const testTour = new Tour({
-  name: 'The Park Camper',
-  rating: 4.7,
-  price: 397,
-});
-
-// Пытаемся сохранить экземпляр как документ в бд монго
-testTour
-  .save()
-  .then((document) => {
-    console.log('document created ->', document);
-  })
-  .catch((err) => console.log('ERROR 💣 ->', err));
-
 /**
  * +---------------+
  * | START SERVER  |
