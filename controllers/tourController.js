8785bbf40be7f667bc946d21be0807c8af19eb2b
SeminVsevolod <semin.vsevolod@gmail.com>
2020-07-15 15:05:53 +0300

    deleting documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -92,11 +92,18 @@ exports.updateTour = async (req, res) => {
 /**
  * Удалить существующий тур
  */
-exports.deleteTour = (req, res) => {
-  // const id = +req.params.id;
-  // const tour = tours.find((el) => el.id === id);
-  // res.status(204).json({
-  //   status: 'success',
-  //   data: null,
-  // });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
 };
